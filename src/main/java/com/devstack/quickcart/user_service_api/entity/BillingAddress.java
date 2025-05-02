7e64885b4b8220d204134765c8771ebebc2b1293
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-02 20:48:52 +0530

    add BillingAddress entity

--- src/main/java/com/devstack/quickcart/user_service_api/entity/BillingAddress.java
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/BillingAddress.java
@@ -1,4 +1,28 @@
 package com.devstack.quickcart.user_service_api.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="billing_address")
 public class BillingAddress {
+    @Id
+    @Column(nullable = false)
+    private String id;
+    @Column(length = 50,nullable = false)
+    private String country;
+    @Column(length = 50,nullable = false)
+    private String city;
+    @Column(length = 50,nullable = false)
+    private String street;
 }
