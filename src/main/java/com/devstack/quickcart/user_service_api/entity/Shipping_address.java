7fed5583c2ecd1747e3ec31ec283daf5d4f5eb0b
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-02 20:37:13 +0530

    add user entity

---
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/Shipping_address.java
@@ -0,0 +1,4 @@
+package com.devstack.quickcart.user_service_api.entity;
+
+public class Shipping_address {
+}
---
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/User.java
@@ -0,0 +1,35 @@
+package com.devstack.quickcart.user_service_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="user")
+
+public class User {
+
+    @Id
+    @Column(unique = true,nullable = false,name="user_id")
+    private String userId;
+    @Column(unique = true,nullable = false,name ="user_name",length = 100)
+    private String userName;
+    @Column(name = "first_name",length = 50,nullable = false)
+    private String firstName;
+    @Column(name = "last_name",length = 50,nullable = false)
+    private String lastName;
+    @Column(name = "active_status",columnDefinition = "TINYINT")
+    private boolean activeState;
+    @Column(name="otp", nullable = false)
+    private int OTP;
+
+}
