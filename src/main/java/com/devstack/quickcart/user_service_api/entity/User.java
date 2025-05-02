d06265e7e04dcd7f9a0d2b47a0af6fbf1e391c68
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-03 01:08:43 +0530

    add UserAvatar entity

--- src/main/java/com/devstack/quickcart/user_service_api/entity/User.java
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/User.java
@@ -1,9 +1,6 @@
 package com.devstack.quickcart.user_service_api.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,5 +28,9 @@ public class User {
     private boolean activeState;
     @Column(name="otp", nullable = false)
     private int OTP;
+
+
+    @OneToOne(mappedBy = "user")
+    private ShippingAddress shippingAddress;
 
 }
--- src/main/java/com/devstack/quickcart/user_service_api/entity/UserAvatar.java
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/UserAvatar.java
@@ -1,4 +1,22 @@
 package com.devstack.quickcart.user_service_api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="user_avatar")
 public class UserAvatar {
+
+    @Id
+    @Column(nullable = false)
+    private String avatarid;
+    @Embedded
+    private FileResource fileResource;
 }
