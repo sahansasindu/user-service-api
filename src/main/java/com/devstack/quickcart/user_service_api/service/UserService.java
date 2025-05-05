2abf00600a70df14dc3381efa973c9434ec32423
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-05 22:33:46 +0530

    add

--- src/main/java/com/devstack/quickcart/user_service_api/service/UserService.java
+++ src/main/java/com/devstack/quickcart/user_service_api/service/UserService.java
@@ -3,14 +3,20 @@ package com.devstack.quickcart.user_service_api.service;
 import com.devstack.quickcart.user_service_api.dto.request.RequestUserDto;
 import com.devstack.quickcart.user_service_api.dto.request.RequestUserLoginRequest;
 import com.devstack.quickcart.user_service_api.dto.request.RequestUserPasswordResetDto;
+import com.devstack.quickcart.user_service_api.dto.response.ResponseUserDto;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface UserService {
 
     public void createUser(RequestUserDto dto)throws IOException;
     public boolean verifyEmail(String otp,String email);
     public Object userLogin(RequestUserLoginRequest request);
+    public List<ResponseUserDto>findUsersPaginate(String searchText,int page,int size);
     public boolean verifyReset(String otp,String email);
     public boolean passwordReset(RequestUserPasswordResetDto resetDto);
+    String getUserId(String email);
+
+
 }
