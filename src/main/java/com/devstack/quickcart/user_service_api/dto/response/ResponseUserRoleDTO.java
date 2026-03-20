e67bce2a237e6f8583d356f480002321da076467
Sahan Sasindu <119693671+sahansasindu@users.noreply.github.com>
2026-03-20 15:43:53 +0530

    update code

--- src/main/java/com/devstack/quickcart/user_service_api/dto/response/ResponseUserRoleDTO.java
+++ src/main/java/com/devstack/quickcart/user_service_api/dto/response/ResponseUserRoleDTO.java
@@ -1,4 +1,12 @@
 package com.devstack.quickcart.user_service_api.dto.response;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ResponseUserRoleDTO {
+    private String roleName;
 }
