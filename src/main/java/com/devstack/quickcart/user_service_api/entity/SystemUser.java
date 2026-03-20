e4ed6d3b076f75bf139cc731272798e2b79cccc8
Sahan Sasindu <119693671+sahansasindu@users.noreply.github.com>
2026-03-20 15:38:20 +0530

    update code

--- src/main/java/com/devstack/quickcart/user_service_api/entity/SystemUser.java
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/SystemUser.java
@@ -1,4 +1,53 @@
 package com.devstack.quickcart.user_service_api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "system_user")
 public class SystemUser {
+    @Id
+    @Column(name = "property_id", nullable = false, length = 80)
+    private String propertyId;
+
+    @Column(name = "active_state", columnDefinition = "TINYINT", nullable = false)
+    private Boolean activeState;
+
+    @Column(name = "email", unique = true, length = 250, nullable = false)
+    private String email;
+
+    @Column(name = "first_name", length = 45, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 45, nullable = false)
+    private String lastName;
+
+    @Column(name = "is_account_non_expired", columnDefinition = "TINYINT", nullable = false)
+    private Boolean isAccountNonExpired;
+
+    @Column(name = "is_email_verified", columnDefinition = "TINYINT", nullable = false)
+    private Boolean isEmailVerified;
+
+    @Column(name = "is_account_non_locked", columnDefinition = "TINYINT", nullable = false)
+    private Boolean isAccountNonLocked;
+
+    @Column(name = "is_enabled", columnDefinition = "TINYINT", nullable = false)
+    private Boolean isEnabled;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date", nullable = false, columnDefinition = "DATETIME")
+    private Date createdDate;
+
+    @OneToOne(mappedBy = "systemUser", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private Otp otp;
+
+    @OneToOne(mappedBy = "systemUser", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private SystemUserAvatar systemUserAvatar;
 }
--- src/main/java/com/devstack/quickcart/user_service_api/entity/SystemUserAvatar.java
+++ src/main/java/com/devstack/quickcart/user_service_api/entity/SystemUserAvatar.java
@@ -1,4 +1,44 @@
 package com.devstack.quickcart.user_service_api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "system_user_avatar")
 public class SystemUserAvatar {
+    @Id
+    @Column(name = "property_id", nullable = false, length = 80)
+    private String propertyId;
+
+    @Lob
+    @Column(name = "directory", nullable = false)
+    private byte[] directory;
+
+    @Lob
+    @Column(name = "file_name", nullable = false)
+    private byte[] fileName;
+
+    @Lob
+    @Column(name = "resource_url", nullable = false)
+    private byte[] resourceUrl;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date", nullable = false, columnDefinition = "DATETIME")
+    private Date createdDate;
+
+    @Lob
+    @Column(name = "hash", nullable = false)
+    private byte[] hash;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_property_id", referencedColumnName = "property_id", nullable = false)
+    private SystemUser systemUser;
 }
